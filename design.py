387eeec1b7109f178e2fd5b4c03cc840f22feacf
Anna <CodeWithAnna@gmail.com>
2021-02-25 20:12:48 +0530

    Commit

--- design.py
+++ design.py
@@ -1,21 +1,18 @@
-# import turtle
-# a=0
-# b=0
-# turtle.bgcolor("black")
-# turtle.speed(0)
-# turtle.pencolor("red")
-# turtle.penup()
-# turtle.goto(0,200)
-# turtle.pendown()
+import turtle
+a=0
+b=0
+turtle.bgcolor("black")
+turtle.speed(0)
+turtle.pencolor("red")
+turtle.penup()
+turtle.goto(0,200)
+turtle.pendown()
 
-# while True:
-#     turtle.backward(a)
-#     turtle.left(b)
-#     a+=3
-#     b+=1
-#     if b==201:
-#         break
-# turtle.close()    
-
-# import numpy as np
-import cv2
+while True:
+    turtle.backward(a)
+    turtle.left(b)
+    a+=3
+    b+=1
+    if b==201:
+        break
+turtle.close()    
